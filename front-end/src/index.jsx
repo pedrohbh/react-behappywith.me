19e71b25352311e200aa77c4115541b9ae9bbfa4
Pedro Hoppe <pedrohbh@github.com>
2021-08-12 15:01:59 -0300

    Terminei a página 125

---
+++ front-end/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './img/favicon.ico';
+
+ReactDOM.render(
+    <h1>Bem-vindo ao React!</h1>,
+    document.querySelector("#main")
+)
