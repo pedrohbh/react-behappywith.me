e8c82883ed803cddb0fb80274d2abc39d00adf55
Pedro Hoppe <pedrohbh@github.com>
2021-08-14 13:59:46 -0300

    Fim da seção 4.5

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Label from '../Label';
 
 
 class NovoUsuario extends React.Component {
     render() {
         return (
             <div className="center">
-
+                <form>
+                    <Label htmlFor="nome" texto="Quem é você?" valorInvalido />
+                </form>
             </div>
         );
     }
