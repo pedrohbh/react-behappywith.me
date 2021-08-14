b74c5054285f3b90f8d30b436b1ca3d26001c77d
Pedro Hoppe <pedrohbh@github.com>
2021-08-14 13:40:32 -0300

    Fim da seção 4.3

---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Componente App!</h1>
+        );        
+    }
+}
+
+export default App;
--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './img/favicon.ico';
 import './css/index.css';
 import './css/pure-min.css';
+import App from './components/App.jsx';
 
 ReactDOM.render(
-    <h1>Bem-vindo ao React!</h1>,
+    <App />,
     document.querySelector("#main")
 )
