a912f3c15f7e059fdd72e65d40139c7526f8f75d
Pedro Hoppe <pedrohbh@github.com>
2021-08-14 13:48:18 -0300

    Fim da página 185

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import NovoUsuario from './NovoUsuario';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Componente App!</h1>
-        );        
+            <div>
+                <Header />
+                <NovoUsuario />
+            </div>
+        );
     }
 }
 
