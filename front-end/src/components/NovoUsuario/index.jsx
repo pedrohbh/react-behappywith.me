21859d08dae356b407fc28963a4b5d992343cf62
Pedro Hoppe <pedrohbh@github.com>
2021-08-15 18:52:47 -0300

    Fim da seção 5.2 - BETA

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import Input from '../Input';
 import Label from '../Label';
 
 
 class NovoUsuario extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuario: {
+                nome: ''
+            },
+            validacao: {
+                nomeInvalido: false
+            }
+        };
+    }
+
+    atualizarNome(e) {
+        let usuario = this.state.usuario;
+        usuario.nome = e.target.value;
+        this.setState({
+            usuario: usuario
+        });
+    }
+
+
     render() {
         return (
             <div className="center">
                 <form className="pure-form pure-form-stacked">
-                    <Label htmlFor="nome" texto="Quem é você?" valorInvalido />
+                    <Label
+                        htmlFor="nome"
+                        texto="Quem é você?"
+                        valorInvalido={this.state.nomeInvalido}
+                    />
+                    <Input
+                        id="nome"
+                        placeholder="Digite seu nome"
+                        maxLength="40"
+                        readOnly={false}
+                        valorInvalido={this.state.nomeInvalido}
+                        defaultValue={this.state.usuario.nome}
+                        onChange={this.atualizarNome.bind(this)}
+                    />
                 </form>
             </div>
         );
